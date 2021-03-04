d484f765c8715c4090320ab619a24bc15a754c9e
Gabriel Uchoa de Vasconcelos <gabriel.uchoa.vasconcelos@gmail.com>
2021-03-04 16:06:31 -0300

    fix: restrições atualizadas

--- src/main/java/br/com/casamagalhaes/workshop/desafio/model/Itens.java
+++ src/main/java/br/com/casamagalhaes/workshop/desafio/model/Itens.java
@@ -16,22 +16,13 @@ public class Itens {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotEmpty(message = "O nome do produto precisa ser definido.")
+    @NotNull(message = "O nome do produto precisa ser definido.")
     private String descricao;
-    @NotEmpty(message = "O valor do produto deve ser definido.")
+    @NotNull(message = "O valor do produto deve ser definido.")
     private float precoUnitario;
-    @NotEmpty(message = "O Produto deve conter pelo menos 1 unidade.")
+    @NotNull(message = "O Produto deve conter pelo menos 1 unidade.")
     private int quantidade;
 
-    public Itens(@NotNull(message = "O nome do produto precisa ser definido.") String descricao, @NotNull(message = "O valor do produto deve ser definido.") float precoUnitario, @NotNull(message = "O Produto deve conter pelo menos 1 unidade.") int quantidade) {
-        this.descricao = descricao;
-        this.precoUnitario = precoUnitario;
-        this.quantidade = quantidade;
-    }
-
-    public Itens() {
-
-    }
 
     public float valorTotal(){
         return precoUnitario * quantidade;
