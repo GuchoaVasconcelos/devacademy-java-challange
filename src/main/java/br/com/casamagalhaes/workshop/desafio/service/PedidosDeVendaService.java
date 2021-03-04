e0d9aeab071f69131a60576f16ffc376be873a5a
Gabriel Uchoa de Vasconcelos <gabriel.uchoa.vasconcelos@gmail.com>
2021-03-04 16:05:08 -0300

    refactor: atualização dos nomes das funções

--- src/main/java/br/com/casamagalhaes/workshop/desafio/service/PedidosDeVendaService.java
+++ src/main/java/br/com/casamagalhaes/workshop/desafio/service/PedidosDeVendaService.java
@@ -3,17 +3,16 @@ package br.com.casamagalhaes.workshop.desafio.service;
 
 import br.com.casamagalhaes.workshop.desafio.model.PedidosDeVenda;
 import org.springframework.data.domain.Page;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 public interface PedidosDeVendaService {
 
-    PedidosDeVenda addPedido(PedidosDeVenda novoPedido);
-    void rmvPedido(Long id);
-    PedidosDeVenda attPedido(Long idPedidoAntigo, PedidosDeVenda novoPedido);
+    PedidosDeVenda adicionar(PedidosDeVenda novoPedido);
+    void remover(Long id);
+    void atualizar(Long idPedidoAntigo, PedidosDeVenda novoPedido);
     List<PedidosDeVenda> listarPedidos();
     Page<PedidosDeVenda> listarPedidosPaginados(Integer numeroPagina, Integer tamanhoPagina);
     PedidosDeVenda listarPedido(Long id);
-    boolean attStatus(Long id, String status);
+    boolean atualizarStatus(Long id, String status);
 }
