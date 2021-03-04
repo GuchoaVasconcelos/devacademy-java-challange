6302fbbea3e98c85d40ca0a9cc168f3fef33af30
Gabriel Uchoa de Vasconcelos <gabriel.uchoa.vasconcelos@gmail.com>
2021-03-04 10:07:23 -0300

    refactor: pedidos de venda service atualizado

--- src/main/java/br/com/casamagalhaes/workshop/desafio/service/PedidosDeVendaService.java
+++ src/main/java/br/com/casamagalhaes/workshop/desafio/service/PedidosDeVendaService.java
@@ -2,13 +2,18 @@ package br.com.casamagalhaes.workshop.desafio.service;
 
 
 import br.com.casamagalhaes.workshop.desafio.model.PedidosDeVenda;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+@Service
 public interface PedidosDeVendaService {
 
     PedidosDeVenda addPedido(PedidosDeVenda novoPedido);
     void rmvPedido(Long id);
-    PedidosDeVenda attPedido(PedidosDeVenda antigoPedido, PedidosDeVenda novoPedido);
-    PedidosDeVenda listarPedidos();
+    PedidosDeVenda attPedido(Long idPedidoAntigo, PedidosDeVenda novoPedido);
+    List<PedidosDeVenda> listarPedidos();
+    Page<PedidosDeVenda> listarPedidosPaginados(Integer numeroPagina, Integer tamanhoPagina);
+    PedidosDeVenda listarPedido(Long id);
+    boolean attStatus(Long id, String status);
 }
