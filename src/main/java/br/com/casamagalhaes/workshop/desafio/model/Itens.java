fdc9c9a55c32cdcbf866a59b5973ba3a19c7f9b0
Gabriel Uchoa de Vasconcelos <gabriel.uchoa.vasconcelos@gmail.com>
2021-03-04 10:25:30 -0300

    feat: adicionado restrições do itens model

--- src/main/java/br/com/casamagalhaes/workshop/desafio/model/Itens.java
+++ src/main/java/br/com/casamagalhaes/workshop/desafio/model/Itens.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Entity
@@ -14,7 +16,24 @@ public class Itens {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty(message = "O nome do produto precisa ser definido.")
     private String descricao;
+    @NotEmpty(message = "O valor do produto deve ser definido.")
     private float precoUnitario;
+    @NotEmpty(message = "O Produto deve conter pelo menos 1 unidade.")
     private int quantidade;
+
+    public Itens(@NotNull(message = "O nome do produto precisa ser definido.") String descricao, @NotNull(message = "O valor do produto deve ser definido.") float precoUnitario, @NotNull(message = "O Produto deve conter pelo menos 1 unidade.") int quantidade) {
+        this.descricao = descricao;
+        this.precoUnitario = precoUnitario;
+        this.quantidade = quantidade;
+    }
+
+    public Itens() {
+
+    }
+
+    public float valorTotal(){
+        return precoUnitario * quantidade;
+    }
 }
