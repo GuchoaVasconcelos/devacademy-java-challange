427f0ef80db86cb07ce1bb24b03586cc010fdd5d
Gabriel Uchoa de Vasconcelos <gabriel.uchoa.vasconcelos@gmail.com>
2021-03-04 10:08:42 -0300

    refactor: pedidos de venda service atualizado

--- src/main/java/br/com/casamagalhaes/workshop/desafio/service/impl/PedidosDeVendaImpl.java
+++ src/main/java/br/com/casamagalhaes/workshop/desafio/service/impl/PedidosDeVendaImpl.java
@@ -4,24 +4,26 @@ import br.com.casamagalhaes.workshop.desafio.model.PedidosDeVenda;
 import br.com.casamagalhaes.workshop.desafio.repository.PedidosDeVendaRepository;
 import br.com.casamagalhaes.workshop.desafio.service.PedidosDeVendaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import javax.persistence.Id;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
 
 @Service
-public class PedidosDeVendaImpl implements PedidosDeVendaService {
+    public class PedidosDeVendaImpl implements PedidosDeVendaService {
 
     @Autowired
     private PedidosDeVendaRepository repository;
 
     @Override
     public PedidosDeVenda addPedido(PedidosDeVenda novoPedido) {
-        return repository.save(novoPedido);
+        return repository.saveAndFlush(novoPedido);
 
     }
-
-
 
     @Override
     public void rmvPedido(Long removerPedido) {
@@ -29,12 +31,64 @@ public class PedidosDeVendaImpl implements PedidosDeVendaService {
     }
 
     @Override
-    public PedidosDeVenda attPedido(PedidosDeVenda antigoPedido, PedidosDeVenda novoPedido) {
-    repository.findById(antigoPedido);
+    public PedidosDeVenda attPedido(Long idPedidoAntigo, PedidosDeVenda novoPedido) {
+        if(repository.existsById(idPedidoAntigo)){
+            if(idPedidoAntigo.equals(novoPedido.getId())){
+                return repository.saveAndFlush(novoPedido);
+            }else{
+                throw new UnsupportedOperationException("Pedido id: " + novoPedido.getId());
+            }
+        }
+        else{
+            throw new EntityNotFoundException("Produto id: " + novoPedido.getId());
+        }
     }
 
     @Override
-    public PedidosDeVenda listarPedidos() {
-        return null;
+    public List<PedidosDeVenda> listarPedidos() {
+        return repository.findAll();
+    }
+
+
+    public Page<PedidosDeVenda> listarPedidosPaginados(Integer numeroPagina, Integer tamanhoPagina){
+        Pageable pageable = PageRequest.of(numeroPagina, tamanhoPagina);
+        return repository.findAll(pageable);
+    }
+
+    @Override
+    public PedidosDeVenda listarPedido(Long id) {
+        return repository.findById(id).orElseThrow(EntityNotFoundException::new);
+    }
+
+
+    @Override
+    public boolean attStatus(Long id, String novoStatus) {
+        PedidosDeVenda pedidosDeVenda = repository.findById(id).orElseThrow(EntityNotFoundException::new);
+        if(novoStatus.equals("CANCELADO")){
+            if(pedidosDeVenda.getStatus().equals("EM_ROTA")||
+               pedidosDeVenda.getStatus().equals("ENTREGUE" )||
+               pedidosDeVenda.getStatus().equals("CANCELADO")){
+                return false;
+            }else{
+                pedidosDeVenda.setStatus(novoStatus);
+            }
+        }
+        else if(novoStatus.equals("EM_ROTA")){
+            if(pedidosDeVenda.getStatus().equals("PRONTO")){
+                pedidosDeVenda.setStatus(novoStatus);
+            }else{
+                return false;
+            }
+        }
+        else if(novoStatus.equals("ENTREGUE")){
+            if(pedidosDeVenda.getStatus().equals("EM_ROTA")){
+                pedidosDeVenda.setStatus(novoStatus);
+            }else{
+                return false;
+            }
+        }
+
+        pedidosDeVenda.setStatus(novoStatus);
+        return true;
     }
 }
