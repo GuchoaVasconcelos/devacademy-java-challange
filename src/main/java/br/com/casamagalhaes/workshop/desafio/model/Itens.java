8c6ab0e5b92b199214e7af096102187dbc365d78
Gabriel Uchoa de Vasconcelos <gabriel.uchoa.vasconcelos@gmail.com>
2021-03-04 22:54:21 -0300

    feat: implementado constructor de itens

--- src/main/java/br/com/casamagalhaes/workshop/desafio/model/Itens.java
+++ src/main/java/br/com/casamagalhaes/workshop/desafio/model/Itens.java
@@ -1,13 +1,11 @@
 package br.com.casamagalhaes.workshop.desafio.model;
 
-
 import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Data
@@ -19,12 +17,27 @@ public class Itens {
     @NotNull(message = "O nome do produto precisa ser definido.")
     private String descricao;
     @NotNull(message = "O valor do produto deve ser definido.")
-    private float precoUnitario;
+    private Double precoUnitario;
     @NotNull(message = "O Produto deve conter pelo menos 1 unidade.")
     private int quantidade;
 
+    public Itens() {
+    }
 
-    public float valorTotal(){
+    public Itens(@NotNull(message = "O nome do produto precisa ser definido.") String descricao, @NotNull(message = "O valor do produto deve ser definido.") Double precoUnitario, @NotNull(message = "O Produto deve conter pelo menos 1 unidade.") int quantidade) {
+        this.descricao = descricao;
+        this.precoUnitario = precoUnitario;
+        this.quantidade = quantidade;
+    }
+
+    public Itens(Long id, @NotNull(message = "O nome do produto precisa ser definido.") String descricao, @NotNull(message = "O valor do produto deve ser definido.") Double precoUnitario, @NotNull(message = "O Produto deve conter pelo menos 1 unidade.") int quantidade) {
+        this.id = id;
+        this.descricao = descricao;
+        this.precoUnitario = precoUnitario;
+        this.quantidade = quantidade;
+    }
+
+    public double valorTotal() {
         return precoUnitario * quantidade;
     }
 }
